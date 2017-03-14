07d7428939b7f3f9694e751a19cbdc86d0f9040f
Kunio <marcosflorentin1@hotmail.com>
2017-03-14 20:37:15 -0300

    sadsa

---
+++ MegaManager/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MegaManager
+{
+    public class TirandoBurros
+    {
+        private void transar()
+        {
+            //lo que pasa es que la vaca esta borracha
+        }
+    }
+}
